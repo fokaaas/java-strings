644d306dead83b3c683ed1e222b00d046421af13
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-10-12 01:11:31 +0300

    chore(Main): added entry point

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get("text.txt"));
+        StringBuffer text = new StringBuffer(new String(bytes));
+        TextProcessor textProcessor = new TextProcessor(text);
+        StringBuffer uniqueWord = textProcessor.findUniqueWord();
+        System.out.println("The first unique word in the text is: " + uniqueWord);
     }
 }
