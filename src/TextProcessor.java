a56d53fe099123348e02393c4e1698dc16a12f58
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-10-12 00:53:53 +0300

    feat(TextProcessor): implemented finding of unique word

--- src/TextProcessor.java
+++ src/TextProcessor.java
@@ -11,11 +11,21 @@ public class TextProcessor {
         char[] sentenceDelimiters = {'.', '!', '?'};
         char[] wordDelimiters = {' ', ',', ':'};
         ArrayList<StringBuffer> sentences = split(text, sentenceDelimiters);
-
+        ArrayList<StringBuffer> firstSentenceWords = split(sentences.getFirst(), wordDelimiters);
+        ArrayList<StringBuffer> otherSentenceWords = new ArrayList<>();
+        for (int i = 1; i < sentences.size(); i++) {
+            ArrayList<StringBuffer> wordsInSentence = split(sentences.get(i), wordDelimiters);
+            otherSentenceWords.addAll(wordsInSentence);
+        }
+        for (StringBuffer word : firstSentenceWords) {
+            if (isUniqueInOtherSentences(word, otherSentenceWords)) {
+                return word;
+            }
+        }
         return null;
     }
 
-    private  ArrayList<StringBuffer> split (StringBuffer text, char[] delimiters) {
+    private ArrayList<StringBuffer> split (StringBuffer text, char[] delimiters) {
         ArrayList<StringBuffer> parts = new ArrayList<>();
         int start = 0;
         for (int i = 0; i < text.length(); i++) {
@@ -37,5 +47,16 @@ public class TextProcessor {
             if (c == delimiter) return true;
         }
         return false;
+    }
+
+    private boolean isUniqueInOtherSentences(StringBuffer word, ArrayList<StringBuffer> otherSentenceWords) {
+        for (StringBuffer otherWord : otherSentenceWords) {
+            if (equalsIgnoreCase(word, otherWord)) return false;
+        }
+        return true;
+    }
+
+    private boolean equalsIgnoreCase(StringBuffer sb1, StringBuffer sb2) {
+        return sb1.toString().equalsIgnoreCase(sb2.toString());
     }
 }
