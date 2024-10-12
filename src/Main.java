55f1b77f9ee579fa8703d60da0e30cfa4a8e5e97
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-10-12 20:26:35 +0300

    feat(TextProcessor): added exception handling

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        byte[] bytes = Files.readAllBytes(Paths.get("text.txt"));
+    public static void main(String[] args) throws RuntimeException {
+        byte[] bytes;
+        try {
+            bytes = Files.readAllBytes(Paths.get("text.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file: " + e.getMessage());
+        }
+
         StringBuffer text = new StringBuffer(new String(bytes));
-        TextProcessor textProcessor = new TextProcessor(text);
-        StringBuffer uniqueWord = textProcessor.findUniqueWord();
-        System.out.println("The first unique word in the text is: " + uniqueWord);
+
+        try {
+            TextProcessor textProcessor = new TextProcessor(text);
+            StringBuffer uniqueWord = textProcessor.findUniqueWord();
+            System.out.println("The first unique word in the text is: " + uniqueWord);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error finding unique word: " + e.getMessage());
+        }
     }
 }
